460544c22661a9b709d6b3c479ef7d19d4f891fd
Anushan <anushan2020@outlook.com>
2023-08-15 20:48:18 +0530

    second

---
+++ condtion.js
@@ -0,0 +1,27 @@
+var a=10;
+var b=30;
+
+if(a==b){
+    console.log("a is equal to b")
+}
+else if(a>b){
+    console.log("a is greater tha b")
+}
+else{
+    console.log("b is greater than b")
+}
+
+
+
+//Logical operator
+
+var email=true;
+var password=true;
+
+if (email || password){
+    console.log("email successs");
+}
+else{
+    console.log("not sucess");
+}
+ 
---
+++ operator.js
@@ -0,0 +1,31 @@
+
+//Arithmetic Operators
+
+
+var a=10;
+var b=40;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a%b);
+console.log(a++);
+console.log(a--)
+
+
+
+//Assignment Operators
+
+a=10;
+b=20;
+if(a<b){
+    console.log("a is lesser than b")
+}
+else{
+    console.log("b is greater than a")
+}
+
+var firstname="vijay";
+var lastname="kumar";
+
+console.log(firstname+" "+lastname);
