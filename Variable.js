ea596aa2b45d9467544896a9f3c5739f2acc7234
Anushan <anushan2020@outlook.com>
2023-08-15 11:01:52 +0530

    firdt js

---
+++ Variable.js
@@ -0,0 +1,14 @@
+var a="sam";
+var b="curran"
+var payment;
+payment="credit";
+
+console.log(a);
+console.log(b);
+console.log(a+b);
+console.log(a+" "+b);
+console.log(payment)
+
+
+
+
---
+++ firstusersignup.js
@@ -0,0 +1,12 @@
+var fullname="sam";
+var email="sam@gmail.com";
+var password="123";
+var confrimpassword="123";
+ 
+
+console.log(`
+fullname:${fullname}
+email:${email}
+password:${password}
+confrimpassword:${confrimpassword}
+`)
