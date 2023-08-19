d87337f9c73fd788a3c1474b7dc42e39562b5c68
Anushan <anushan2020@outlook.com>
2023-08-19 13:15:55 +0530

    about undefined

---
+++ Truthyandflasy.js
@@ -0,0 +1,25 @@
+//undefined
+//null
+//Nan
+//0
+
+
+
+
+
+var user=null;
+if(user){
+    console.log("user has value")
+}
+else{
+    console.log("user has no value")
+}
+
+//null is string
+var user1="null"
+if(user1){
+    console.log("user has value")
+}
+else{
+    console.log("user has no value")
+}
