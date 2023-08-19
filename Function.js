39d4ced86f103e5d00f5ee60f1e3149cad875a20
Anushan <anushan2020@outlook.com>
2023-08-19 15:44:05 +0530

    function

--- Function.js
+++ Function.js
@@ -1,5 +1,17 @@
-function firstfun(){
+function firstfun(name){
     console.log("my first function");
+    console.log("new fun"+" "+name);
+    console.log(`welcome to javascript ${name} have fun`)
 }
 
-firstfun();
+
+//firstfun();
+firstfun("anushan");
+
+
+function secondfun(){
+    return "my second function";
+}
+
+var funvariable=secondfun();
+console.log(funvariable);
