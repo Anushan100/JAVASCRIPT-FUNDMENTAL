4d8f0167409f6149a830fd05b9dc014fd534043d
Anushan <anushan2020@outlook.com>
2023-08-19 12:52:47 +0530

    first

---
+++ switchcase.js
@@ -0,0 +1,15 @@
+var user="admin"
+
+switch(user){
+    case "admin":
+        console.log("i am admin");
+        break;
+    case "subadmin":
+        console.log("i am subadmin")
+        break;
+    case "user":
+        console.log("i am user");
+        break;
+    default:
+        console.log("Trial user")
+}
