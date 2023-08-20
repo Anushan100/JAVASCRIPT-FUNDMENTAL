bb3cb7dd522333feace526ef7259d61ebd1bc5a4
Anushan <anushan2020@outlook.com>
2023-08-20 13:02:38 +0530

    Array and methods

---
+++ Array.js
@@ -0,0 +1,13 @@
+var array=["india","canada","pakistan","srilanka"]
+console.log(array)
+console.log(array[1])
+console.log(array.length)
+array[0]="iceland"
+console.log("array=======",array)
+array.pop();
+console.log(array)
+console.log(array.indexOf("iceland"));
+
+console.log(Array.from("vijaykumar"))
+
+//var name="vijaykumar"
