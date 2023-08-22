99760235e023ef2b1df571459a013e314a18f692
Anushan <anushan2020@outlook.com>
2023-08-22 20:25:44 +0530

    object

---
+++ object.js
@@ -0,0 +1,11 @@
+var user={
+    firstname:"vijay",
+    Lastname:"kumar",
+    age:"43",
+    count:"100"
+}
+
+user.count=1000;
+console.log(user.age);
+console.log(user["age"])
+console.table(user)
