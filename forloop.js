5a72a8a5300e9aeb16e50cd4862c80c8f8b514e3
Anushan <anushan2020@outlook.com>
2023-09-02 12:40:23 +0530

    first forloop

---
+++ forloop.js
@@ -0,0 +1,14 @@
+//for(i=0;i<=10;i++){
+    //console.log(i)
+//}
+
+
+
+const states=["tamilnadu","kerala","mumbai",1947]
+for( let i=0;i<states.length;i++){
+    if(typeof states[i]=="string"){
+        console.log(states[i]);
+         continue;
+    }
+}
+
