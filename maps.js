9e611c3adb604f0f8cc4583bb7c557ecc2873136
Anushan <anushan2020@outlook.com>
2023-09-22 20:29:15 +0530

    maps

---
+++ maps.js
@@ -0,0 +1,11 @@
+var myMap=new Map();
+myMap.set(1,"vijay")
+myMap.set(2,"Ajith")
+myMap.set(3,"kamal")
+myMap.set(4,"Rajini")
+myMap.set(5,"surya")
+console.log(myMap)
+
+
+
+myMap.forEach((key)=>console.log(key))
